ceccf7e404f321422ceb5df3c54b6e315f855906
XIO <100460377+TazkieCT@users.noreply.github.com>
2025-07-01 12:48:45 +0700

    Chore: redirect to dashboard after successfullly upload

--- upload.php
+++ upload.php
@@ -89,7 +89,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $existingData[] = $videoData;
 
                 file_put_contents($jsonFile, json_encode($existingData, JSON_PRETTY_PRINT));
-
+                header('Location: /ZouTube/dashboard.php');
+                exit;
             } else {
                 $errors[] = 'Gagal menyimpan file.';
             }
@@ -141,7 +142,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <ul>
                     <li>
                         <a href="/ZouTube/dashboard.php">
-                            <svg viewBox="0 0 24 24" width="24" height="24">
+                            <svg width="24" height="24" viewBox="0 0 24 24">
                                 <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
                             </svg>
                             <span>Content</span>
@@ -149,7 +150,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     </li>
                     <li class="active">
                         <a href="/ZouTube/upload.php">
-                            <svg viewBox="0 0 24 24" width="24" height="24">
+                            <svg width="24" height="24" viewBox="0 0 24 24">
                                 <path d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4zM14 13h-3v3H9v-3H6v-2h3V8h2v3h3v2z"></path>
                             </svg>
                             <span>Upload Video</span>
